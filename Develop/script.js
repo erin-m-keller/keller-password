0d39d53b2a671104a9af27a19d19903cd430a255
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-02-06 12:23:20 -0500

    added functionality to generate the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,28 @@
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+/* initialize variables */
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+var numericArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var generatedArr = [];
 
-
-
-/* Get references to the #generate element */
+/* get references to the #generate element */
 var generateBtn = document.querySelector("#generate");
+
+/* add event listener to generate button */
+generateBtn.addEventListener("click", writePassword);
 
 /* get character length */
 function getCharLength() {
 
   /* initialize prompt */
-  let charLength = parseInt(prompt('Enter the number of characters you would like for your password. (Minimum 8 - Maximum 128)'))
+  var charLength = parseInt(prompt('Enter the number of characters you would like for your password. (Minimum 8 - Maximum 128)'))
   
   /* display prompt until user enters a valid character limit */
   while(charLength < 8 || charLength > 128) {
     alert("You must select a character limit with a minimum of 8 characters, or a maximum of 128.")
     charLength = parseInt(prompt('Enter the number of characters you would like for your password. (Minimum 8 - Maximum 128)'));
   }
-  
+
   /* return the users choice to the original function that triggered this function */
   return charLength;
 }
@@ -38,13 +30,15 @@ function getCharLength() {
 function getCharType() {
 
   /* initialize prompts */
-  let charFlag = false;
+  var charFlag = false;
+
   /* display prompt until user has selected at least one character type */
   while (charFlag === false) {
     lowerChar = prompt('Would you like lowercase characters? (Y/N)');
     upperChar = prompt('Would you like uppercase characters? (Y/N)');
     numericChar = prompt('Would you like numeric characters? (Y/N)');
     specialChar = prompt('Would you like special characters? (Y/N)');
+
     /* check if one character type is selected, if it is then break the loop. otherwise, display alert and prompts again */
     if (lowerChar.toUpperCase() != "Y" && upperChar.toUpperCase() != "Y" && numericChar.toUpperCase() != "Y" && specialChar.toUpperCase() != "Y") {
       alert("You must select at least one character type to continue.")
@@ -54,46 +48,108 @@ function getCharType() {
     }
   }
   /* return the users choice to the original function that triggered this function */
-  let charArr = [
+  var charArr = [
     {
       name: "lowerCase",
-      value: lowerChar
+      value: lowerChar.toUpperCase()
     },
     {
       name: "upperCase",
-      value: upperChar
+      value: upperChar.toUpperCase()
     },
     {
       name: "numeric",
-      value: numericChar
+      value: numericChar.toUpperCase()
     },
     {
       name: "special",
-      value: specialChar
+      value: specialChar.toUpperCase()
     }
   ];
   return charArr;
 }
 
+/**
+ * shuffles array in place
+ * referenced shuffle function
+ * from stackoverflow:
+ * https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ */
+ function shuffle(a) {
+  for (var i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
 /* generate the password based on user input: character length, lower/upper/numeric/special */
 function generatePassword() {
 
-  let charLength = getCharLength();
-  let charType = getCharType();
-  let lowerCaseFlag = charType[0].value;
-  let upperCaseFlag = charType[1].value;
-  let numericFlag = charType[2].value;
-  let specialCharFlag = charType[3].value;
+  var charLength = getCharLength();
+  var charType = getCharType();
+  var lowerCaseFlag = charType[0].value;
+  var upperCaseFlag = charType[1].value;
+  var numericFlag = charType[2].value;
+  var specialCharFlag = charType[3].value;
+  var numElemsNeed = charLength - generatedArr.length;
+  /* 
+   * referenced Math.floor(Math.random() * array.length); 
+   * from stackoverflow: 
+   * https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array 
+   * 
+   */
 
+  /* pull a random numeric value from the numeric array if user wanted a numeric value and add to generated array */
+  if (numericFlag === "Y") {
+    var randomNumeric = Math.floor(Math.random() * numericArr.length);
+    var numericVal = numericArr[randomNumeric];
+    generatedArr.push(numericVal);
+  }
 
+  /* pull a random special character from the special array if user wanted a special character and add to generated array */
+  if (specialCharFlag === "Y") {
+    var randomSpecial = Math.floor(Math.random() * specialArr.length);
+    var specialVal = specialArr[randomSpecial];
+    generatedArr.push(specialVal);
+  }
+
+  if (lowerCaseFlag === "Y" && upperCaseFlag === "Y") {
+    /* generate random lowercase values for half the needed number of characters */
+    for (var i = 0; i < (numElemsNeed / 2); i++) {
+      var lowerCase = Math.floor(Math.random() * lowerArr.length);
+      var lowerVal = lowerArr[lowerCase];
+      generatedArr.push(lowerVal);
+    }
+    /* generate random uppercase values for half the needed number of characters */
+    for (var j = 0; j < (numElemsNeed / 2); j++) {
+      var upperCase = Math.floor(Math.random() * upperArr.length);
+      var upperVal = upperArr[upperCase];
+      generatedArr.push(upperVal);
+    }
+  } else if (lowerCaseFlag === "Y" && upperCaseFlag === "N") {
+    /* generate random lowercase values for the needed number of characters */
+    for (var k = 0; k < numElemsNeed; k++) {
+      var lowerCase = Math.floor(Math.random() * lowerArr.length);
+      var lowerVal = lowerArr[lowerCase];
+      generatedArr.push(lowerVal);
+    }
+  } else {
+    /* generate random uppercase values for the needed number of characters */
+    for (var l = 0; l < numElemsNeed; l++) {
+      var upperCase = Math.floor(Math.random() * upperArr.length);
+      var upperVal = upperArr[upperCase];
+      generatedArr.push(upperVal);
+    }
+  }
+  /* shuffle array and return string to write password function */
+  var password = shuffle(generatedArr);
+  return password.join("");
 }
 
-/* Write password to the #password input */
+/* write password to the #password input */
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
-/* Add event listener to generate button */
-generateBtn.addEventListener("click", writePassword);
